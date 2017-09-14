9622a114e5a8cafda8c147ce4b0f0f105fca6b82
Peitian Xiong <peitian.xiong@berkeley.edu>
2017-09-14 15:28:37 -0700

    finish ex2

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		uint a = 0;
+		uint b = 1;
+		if (position == 0)
+			return a;
+		for (uint i = 2; i <= position; i++) {
+			uint c = a + b;
+			a = b;
+			b = c;
+		}
+		return b;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function () {}
 }
